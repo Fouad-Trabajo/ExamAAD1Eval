cb0cfc50e727fc5ed0dc6fcf6fc5f94773c75bb4
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2024-12-02 17:55:31 +0100

    feat: corrección

--- app/src/main/java/edu/iesam/examaad1eval/features/ex1/data/local/xml/XmlEx1LocalDataSource.kt
+++ app/src/main/java/edu/iesam/examaad1eval/features/ex1/data/local/xml/XmlEx1LocalDataSource.kt
@@ -2,13 +2,19 @@ package edu.iesam.examaad1eval.features.ex1.data.local.xml
 
 import android.content.Context
 import com.google.gson.Gson
+import edu.iesam.examaad1eval.R
 import edu.iesam.examaad1eval.features.ex1.domain.User
 
 class XmlEx1LocalDataSource(private val context: Context) {
 
     private val gson = Gson()
 
-    private val sharedPref
+    private val sharedPref =
+        context.getSharedPreferences(
+            context.getString(R.string.db_exam),
+            Context.MODE_PRIVATE
+        )
+
 
     fun saveUsers(users: List<User>) {
 
